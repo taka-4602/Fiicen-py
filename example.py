8f0879dd48e8ee161cca519bb852274973739e2f
taka-4602 <116678390+taka-4602@users.noreply.github.com>
2024-05-22 22:22:44 +0900

    Add files via upload

--- example.py
+++ example.py
@@ -1,10 +1,15 @@
-from .fiicen import Fiicen
+from .main import Fiicen
 
 fiicen=Fiicen(name="ユーザー名",password="パスワード")#ログイン、nameとpasswordを設定しなかったらログインをスキップします
 print(fiicen.fly_circle(contents="メインの文章",vote_choices1="投票の選択肢1",vote_choices2="投票の選択肢2"))
 print(fiicen.change_profile(display_name="表示名",introduce="自己紹介"))
 print(fiicen.follow(followed_id="ユーザーID"))#ユーザーIDの取得方法がリクエスト見るしかなさそうなので使い道ナシ？
 print(fiicen.get_topic())#タイムラインをhtmlで取得する
+print(fiicen.notification())#通知の数をintで返す
+print(fiicen.like(circle_id="サークルID"))#いいね！
+print(fiicen.refly(circle_id="サークルID"))#リフライ
+print(fiicen.fix_circle(circle_id="サークルID"))#プロフィールにサークルを固定
+print(fiicen.report(circle_id="サークルID",type="通報理由"))#サークルを通報、理由は：harassment / sensitive / spam / suicide / spoofing / privacy / violence / misinformation / discrimination から選ぶ
 
 fiicen=Fiicen()#アカウント作成の時はログインをスキップ
 print(fiicen.create_account(name="ユーザー名",display_name="表示名",password="パスワード"))
